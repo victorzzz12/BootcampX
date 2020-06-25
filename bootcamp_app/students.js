2341d54e3b3be8b00ca55802235d4839fefb90c6
Victor Liang <victor.zw.liang@gmail.com>
2020-06-25 02:56:10 +0000

    parameterized queries for security

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -17,12 +17,17 @@ LIMIT 5;
 })
 .catch(err => console.error('query error', err.stack));
 
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`, limit];
+
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
+WHERE cohorts.name LIKE $1
+LIMIT $2;
 `)
 .then(res => {
   res.rows.forEach(user => {
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,13 +7,16 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
 `)
 .then(res => {
